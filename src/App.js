a2847edd3e6de0549710d28c3adb6d6473b8ca0c
Jordan <administrator@JTGs-MBP.fios-router.home>
2017-10-11 13:35:54 -0400

    add firebase & reducer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBLKcFWFYFZJTWg_25pB40hp4GxUK0pKek',
+      authDomain: 'manager-92ef7.firebaseapp.com',
+      databaseURL: 'https://manager-92ef7.firebaseio.com',
+      projectId: 'manager-92ef7',
+      storageBucket: 'manager-92ef7.appspot.com',
+      messagingSenderId: '1090815616511'
+};
+    firebase.initializeApp(config);
+  }
   render() {
     return (
-      <Provider>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>
             Hello!
